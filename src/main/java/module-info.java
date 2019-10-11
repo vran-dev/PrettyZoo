a673474b11dbe2e4048164bc4f9f6699d84129c4
vran-dev <vran_dev@foxmail.com>
2019-10-11 18:38:07 +0800

    1. upgrade to Java11 2. use log4j2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+open module PrettyZoo.main {
+    requires static lombok;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires curator.framework;
+    requires curator.recipes;
+    requires curator.client;
+    requires zookeeper;
+    requires org.slf4j;
+    requires log4j.slf4j.impl;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.google.common;
+    requires richtextfx;
+    requires flowless;
+    exports cc.cc1234.main;
+    uses org.apache.logging.log4j.message.ThreadDumpMessage.ThreadInfoFactory;
+    uses org.apache.logging.log4j.spi.Provider;
+    uses org.apache.logging.log4j.util.PropertySource;
+}
