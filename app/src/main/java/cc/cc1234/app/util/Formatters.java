f05fa4fff5b274f180fee87be9f253ff29b2e97a
vran-dev <vran_dev@foxmail.com>
2021-01-06 21:57:15 +0800

    feat: refactor xml formatter

--- app/src/main/java/cc/cc1234/app/util/Formatters.java
+++ app/src/main/java/cc/cc1234/app/util/Formatters.java
@@ -2,46 +2,41 @@ package cc.cc1234.app.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.w3c.dom.Node;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.InputSource;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.StringReader;
-import java.io.StringWriter;
 
 public class Formatters {
 
-    public static final TransformerFactory transformerFactory = TransformerFactory.newInstance();
     private static final ObjectMapper jsonMapper = new ObjectMapper();
-
-    static {
-        transformerFactory.setAttribute("indent-number", 2);
-
-    }
 
     public static String prettyJson(String raw) throws JsonProcessingException {
         return jsonMapper.readTree(raw).toPrettyString();
     }
 
-    public static String prettyXml(String raw) throws Exception {
-        return prettyFormat(raw);
+    public static String prettyXml(String raw) {
+        return xmlFormat(raw);
     }
 
-    public static String prettyFormat(String input) {
+    public static String xmlFormat(String xml) {
         try {
-            Source xmlInput = new StreamSource(new StringReader(input));
-            StreamResult xmlOutput = new StreamResult(new StringWriter());
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes");
-            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-            transformer.transform(xmlInput, xmlOutput);
-            return xmlOutput.getWriter().toString();
+            final InputSource src = new InputSource(new StringReader(xml));
+            final Node document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(src).getDocumentElement();
+            final Boolean keepDeclaration = xml.contains("<?xml");
+
+            final DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+            final DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
+            final LSSerializer writer = impl.createLSSerializer();
+            writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE); // Set this to true if the output needs to be beautified.
+            writer.getDomConfig().setParameter("xml-declaration", keepDeclaration); // Set this to true if the declaration is needed to be outputted.
+            return writer.writeToString(document);
         } catch (Exception e) {
-            throw new IllegalStateException(e);
+            throw new RuntimeException(e);
         }
     }
 }
