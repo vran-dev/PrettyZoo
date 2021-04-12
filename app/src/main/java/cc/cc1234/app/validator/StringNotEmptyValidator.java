8aa4e76c53b1338d4af5230e4ff087e79527a7aa
vran <vran_dev@foxmail.com>
2021-04-12 21:01:37 +0800

    feat: migration validator to jfoenix (#145)

---
+++ app/src/main/java/cc/cc1234/app/validator/StringNotEmptyValidator.java
@@ -0,0 +1,21 @@
+package cc.cc1234.app.validator;
+
+import com.jfoenix.validation.base.ValidatorBase;
+import javafx.scene.control.TextInputControl;
+
+public class StringNotEmptyValidator extends ValidatorBase {
+
+    @Override
+    protected void eval() {
+        if (srcControl.get() instanceof TextInputControl) {
+            TextInputControl textField = (TextInputControl) srcControl.get();
+            String alias = textField.getText();
+            if (alias != null && !alias.isEmpty() && alias.isBlank()) {
+                hasErrors.set(true);
+                setMessage("must be all blank");
+            } else {
+                hasErrors.set(false);
+            }
+        }
+    }
+}
